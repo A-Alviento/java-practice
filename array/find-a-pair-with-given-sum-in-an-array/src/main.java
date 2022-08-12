f693f5ff084cdcc56c41a60879d861d8d5722123
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2022-08-12 15:27:48 +0800

    added comments for brute force method

--- array/find-a-pair-with-given-sum-in-an-array/src/main.java
+++ array/find-a-pair-with-given-sum-in-an-array/src/main.java
@@ -4,15 +4,20 @@ public class main {
 	// only returns one of the possible outputs based on what it finds first
 	
 	public static void findPairBruteForce(int[] nums, int target) {
+		// iterates over the array
 		for(int i=0; i<nums.length; i++) {
+			// i is fixed here, and we iterate from index i onwards excluding i
 			for(int j=i+1; j<nums.length; j++) {
+				// compares the sum of the value at index i and j with target value
 				if(nums[i] + nums[j] == target) {
+					// if matches, we print the value pair and end the method
 					System.out.println("Pair found (" + nums[i] + "," + nums[j] + ")");
 					return;
 				}
 			}
 		}
-		
+		// only reaches this line when no suitable pairs found
+		// prints failed message and terminates method
 		System.out.println("No suitable pairs.");
 		return;
 	}
